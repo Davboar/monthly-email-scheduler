c1c91c07f99c29aa9ee5c00388ca6f3f59b37f14
Davboar <davboar@hotmail.com>
2025-11-02 16:48:38 +0100

    Update send_email.py

--- send_email.py
+++ send_email.py
@@ -28,8 +28,8 @@ def list_from_env(name):
 
 def should_send_today(now_utc):
     # Get config
-    target_day = int(env("DAY_OF_MONTH", "23"))  # 1..31
-    run_hour_utc = int(env("RUN_HOUR_UTC", "8")) # 0..23
+    target_day = int(env("DAY_OF_MONTH", "2"))  # 1..31
+    run_hour_utc = int(env("RUN_HOUR_UTC", "16")) # 0..23
     send_on_last_if_short = parse_bool(env("SEND_ON_LAST_IF_SHORT", "true"))
 
     # Only proceed at the chosen hour
